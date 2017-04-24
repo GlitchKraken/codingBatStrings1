fca240725120c0275006c88ebee2510422eda22c
cfrybb <cfry@stu.ctcd.edu>
2017-04-24 17:08:46 -0500

    Solved Challenge: String1 makeOutWord

--- test.cpp
+++ test.cpp
@@ -1,3 +1,12 @@
+/*
+	Chad Fry & Jessy Loy | ITSE 2421   | Prof Welch
+	April 24, 2017
+	Extra Credit points!
+	String Challenges
+
+*/
+
+
 #include <iostream>
 #include <string>
 #include <iomanip>
@@ -8,7 +17,7 @@ using namespace std;
 
 
 string makeTags(string tag, string word);
-
+string makeOutWord(string out, string word);
 
 
 
@@ -17,11 +26,18 @@ int main()
 {
 
 
-	assert(makeTags("i", "Yay") == "<i>Yay</i>");
-	assert(makeTags("i", "Hello") == "<i>Hello</i>");
-	assert(makeTags("cite", "Yay") == "<cite>Yay</cite>");
-	assert(makeTags("b", "Bold") == "<b>Bold</b>");
-	assert(makeTags("I", "Italics") == "<I>Italics</I>");
+	//assert(makeTags("i", "Yay") == "<i>Yay</i>");
+	//assert(makeTags("i", "Hello") == "<i>Hello</i>");
+	//assert(makeTags("cite", "Yay") == "<cite>Yay</cite>");
+	//assert(makeTags("b", "Bold") == "<b>Bold</b>");
+	//assert(makeTags("I", "Italics") == "<I>Italics</I>");
+	assert(makeOutWord("<<>>", "Yay") == "<<Yay>>");
+	assert(makeOutWord("<<>>", "WooHoo") == "<<WooHoo>>");
+	assert(makeOutWord("[[]]", "word") == "[[word]]");
+	assert(makeOutWord("!!!!", "Wow") == "!!Wow!!");
+	assert(makeOutWord("????", "What") == "??What??");
+
+
 
 
 
@@ -36,6 +52,15 @@ string makeTags(string tag, string word)
 
 	result = ("<" + tag + ">" + word + "</" + tag + ">" );
 
+
+	return result;
+}
+
+string makeOutWord(string out, string word)
+{
+	string result;
+
+	result = out.substr(0,2) + word + out.substr(2,2);
 
 	return result;
 }
