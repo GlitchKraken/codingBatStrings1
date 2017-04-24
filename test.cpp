15fdfec88392f6ba394ad31cd679009b1a815364
cfrybb <cfry@stu.ctcd.edu>
2017-04-24 16:57:17 -0500

    Solved Challenge string1 maketags

--- test.cpp
+++ test.cpp
@@ -2,16 +2,40 @@
 #include <string>
 #include <iomanip>
 #include <cstdlib>
+#include <cassert>
 
 using namespace std;
+
+
+string makeTags(string tag, string word);
+
+
+
+
 
 int main()
 {
 
-	cout << "Hello world! :D" << endl;
 
-	cout << "But now it also says hello bob! aha ha!!!" << endl;
+	assert(makeTags("i", "Yay") == "<i>Yay</i>");
+	assert(makeTags("i", "Hello") == "<i>Hello</i>");
+	assert(makeTags("cite", "Yay") == "<cite>Yay</cite>");
+	assert(makeTags("b", "Bold") == "<b>Bold</b>");
+	assert(makeTags("I", "Italics") == "<I>Italics</I>");
+
+
 
 
 	return 0;
+}
+
+
+string makeTags(string tag, string word)
+{
+	string result;
+
+	result = ("<" + tag + ">" + word + "</" + tag + ">" );
+
+
+	return result;
 }
